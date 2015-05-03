dfc7a4dd2b3d14fad52e2321e226c55a98d6570e
Robert Krahn <robert.krahn@gmail.com>
2015-05-02 23:37:50 -0700

    tools.reader.exclusion

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.rksm/cloxp-projects "0.1.6"
+(defproject org.rksm/cloxp-projects "0.1.7"
   :description "Dealing with clojure projects and project configurations."
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
@@ -8,7 +8,7 @@
                  [org.clojure/data.json "0.2.6"]
                  [com.cemerick/pomegranate "0.3.0"]
                  [org.rksm/system-files "0.1.5"]
-                 [leiningen/leiningen "2.5.1"]]
+                 [leiningen/leiningen "2.5.1" :exclusions [org.clojure/tools.reader]]]
   :scm {:url "git@github.com:cloxp/cloxp-projects.git"}
   :pom-addition [:developers [:developer
                               [:name "Robert Krahn"]
