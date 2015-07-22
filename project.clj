4b13286485610a1e44bf9e0a9acef67182a7f545
Robert Krahn <robert.krahn@gmail.com>
2015-07-21 18:26:30 -0700

    lib update

--- project.clj
+++ project.clj
@@ -3,11 +3,12 @@
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :url "http://github.com/cloxp/cloxp-projects"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.xml "0.0.8"]
                  [org.clojure/data.json "0.2.6"]
                  [com.cemerick/pomegranate "0.3.0"]
                  [org.rksm/system-files "0.1.6"]
+                 [classlojure/classlojure "0.6.6"]
                  [leiningen/leiningen "2.5.1" :exclusions [org.clojure/tools.reader]]]
   :profiles {:dev {:dependencies [[org.rksm/test-helpers "0.1.0"]]}}
   :scm {:url "git@github.com:cloxp/cloxp-projects.git"}
