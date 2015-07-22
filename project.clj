43a18a6522b2bd38b42694e124b76a4ff7542859
Robert Krahn <robert.krahn@gmail.com>
2015-07-22 14:39:52 -0700

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.rksm/cloxp-projects "0.1.9"
+(defproject org.rksm/cloxp-projects "0.1.10-SNAPSHOT"
   :description "Dealing with clojure projects and project configurations."
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
@@ -7,7 +7,7 @@
                  [org.clojure/data.xml "0.0.8"]
                  [org.clojure/data.json "0.2.6"]
                  [com.cemerick/pomegranate "0.3.0"]
-                 [org.rksm/system-files "0.1.6"]
+                 [org.rksm/system-files "0.1.7-SNAPSHOT"]
                  [classlojure/classlojure "0.6.6"]
                  [leiningen/leiningen "2.5.1" :exclusions [org.clojure/tools.reader]]]
   :profiles {:dev {:dependencies [[org.rksm/test-helpers "0.1.0"]]}}
